bfe344d76b5e31773a83fdfa1d8307e939fa7d53
Arne Tripolone <arnetripolone@gmail.com>
2021-07-06 10:24:45 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var APIKey = "42c433d4450fe2209bd6a8a993c251fc";
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var city = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var city = document.querySelector('#search-input').value;
+
+  if (!city) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  location.assign(queryURL);
+}
+
+city.addEventListener('submit', handleSearchFormSubmit);
+
+fetch(queryUrl);
+
+
