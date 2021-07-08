e25499d955d709f5465f82591f7017518a5bc211
Arne Tripolone <arnetripolone@gmail.com>
2021-07-08 13:42:57 +1000

    Cards load 5 day forecast!

--- script.js
+++ script.js
@@ -53,23 +53,87 @@ function callweatherAPI(city) {
       return response.json();
     })
     .then((data) => {
-      (console.log(data.list[8].dt_txt, data.list[8].main.temp, data.list[8].main.humidity, data.list[8].wind.speed))
+      (console.log(data.list[6].dt_txt, data.list[6].main.temp, data.list[6].main.humidity, data.list[6].wind.speed))
       
     var citydatef1 = document.createElement("p")
-    citydatef1.textContent = data.list[8].dt_txt
-    document.getElementById("citydatef1").textContent=data.list[8].dt_txt
+    citydatef1.textContent = data.list[6].dt_txt
+    document.getElementById("citydatef1").textContent=data.list[6].dt_txt.slice(0, 10)
     
     var citytempf1 = document.createElement("p")
-    citytempf1.textContent = textContent=data.list[8].main.temp
-    document.getElementById("citytempf1").textContent=data.list[8].main.temp
+    citytempf1.textContent = textContent=data.list[6].main.temp
+    document.getElementById("citytempf1").textContent="Temp: " + data.list[6].main.temp
 
     var citywindf1 = document.createElement("p")
-    citywindf1.textContent = textContent=data.list[8].wind.speed
-    document.getElementById("citywindf1").textContent=data.list[8].wind.speed
+    citywindf1.textContent = textContent=data.list[6].wind.speed
+    document.getElementById("citywindf1").textContent="Wind: " + data.list[6].wind.speed
 
     var cityhumidityf1 = document.createElement("p")
-    cityhumidityf1.textContent = textContent=data.list[8].main.humidity
-    document.getElementById("cityhumidityf1").textContent=data.list[8].main.humidity; 
+    cityhumidityf1.textContent = textContent=data.list[6].main.humidity
+    document.getElementById("cityhumidityf1").textContent="Humidity: " + data.list[6].main.humidity;
+    
+    var citydatef2 = document.createElement("p")
+    citydatef2.textContent = data.list[16].dt_txt
+    document.getElementById("citydatef2").textContent=data.list[14].dt_txt.slice(0, 10)
+    
+    var citytempf2 = document.createElement("p")
+    citytempf2.textContent = textContent=data.list[14].main.temp
+    document.getElementById("citytempf2").textContent="Temp: " + data.list[14].main.temp
+
+    var citywindf2 = document.createElement("p")
+    citywindf2.textContent = textContent=data.list[14].wind.speed
+    document.getElementById("citywindf2").textContent="Wind: " + data.list[14].wind.speed
+
+    var cityhumidityf2 = document.createElement("p")
+    cityhumidityf2.textContent = textContent=data.list[14].main.humidity
+    document.getElementById("cityhumidityf2").textContent="Humidity: " + data.list[14].main.humidity;
+
+    var citydatef3 = document.createElement("p")
+    citydatef3.textContent = data.list[24].dt_txt
+    document.getElementById("citydatef3").textContent=data.list[22].dt_txt.slice(0, 10)
+    
+    var citytempf3 = document.createElement("p")
+    citytempf3.textContent = textContent=data.list[22].main.temp
+    document.getElementById("citytempf3").textContent="Temp: " + data.list[22].main.temp
+
+    var citywindf3 = document.createElement("p")
+    citywindf3.textContent = textContent=data.list[22].wind.speed
+    document.getElementById("citywindf3").textContent="Wind: " + data.list[22].wind.speed
+
+    var cityhumidityf3 = document.createElement("p")
+    cityhumidityf3.textContent = textContent=data.list[22].main.humidity
+    document.getElementById("cityhumidityf3").textContent="Humidity: " + data.list[22].main.humidity;
+
+    var citydatef4 = document.createElement("p")
+    citydatef4.textContent = data.list[8].dt_txt
+    document.getElementById("citydatef4").textContent=data.list[30].dt_txt.slice(0, 10)
+    
+    var citytempf4 = document.createElement("p")
+    citytempf4.textContent = textContent=data.list[30].main.temp
+    document.getElementById("citytempf4").textContent="Temp: " + data.list[30].main.temp
+
+    var citywindf4 = document.createElement("p")
+    citywindf4.textContent = textContent=data.list[30].wind.speed
+    document.getElementById("citywindf4").textContent="Wind: " + data.list[30].wind.speed
+
+    var cityhumidityf4 = document.createElement("p")
+    cityhumidityf4.textContent = textContent=data.list[30].main.humidity
+    document.getElementById("cityhumidityf4").textContent="Humidity: " + data.list[30].main.humidity;
+
+    var citydatef5 = document.createElement("p")
+    citydatef5.textContent = data.list[39].dt_txt
+    document.getElementById("citydatef5").textContent=data.list[38].dt_txt.slice(0, 10)
+    
+    var citytempf5 = document.createElement("p")
+    citytempf5.textContent = textContent=data.list[38].main.temp
+    document.getElementById("citytempf5").textContent="Temp: " + data.list[38].main.temp
+
+    var citywindf5 = document.createElement("p")
+    citywindf5.textContent = textContent=data.list[38].wind.speed
+    document.getElementById("citywindf5").textContent="Wind: " + data.list[38].wind.speed
+
+    var cityhumidityf5 = document.createElement("p")
+    cityhumidityf5.textContent = textContent=data.list[38].main.humidity
+    document.getElementById("cityhumidityf5").textContent="Humidity: " + data.list[38].main.humidity;
     });}
   
 city.addEventListener('submit', handleSearchFormSubmit);
